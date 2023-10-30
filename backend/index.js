33004d5c3dea65a153ea338e92df76adf8c71d1a
nh602 <nhenley602@gmail.com>
2023-10-30 23:50:27 +0000

    imported vendor router

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
+const VendorRouter = require('routes/VendorRouter');
+
 const app = express()
 const port = 3000
+
+app.use('/vendor', VendorRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
