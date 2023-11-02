0762f37b28bde9abede520f2fb94cd693ca4a868
nh602 <nhenley602@gmail.com>
2023-11-02 21:18:07 +0000

    Backend loads environment variables from project root .env file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const app = express();
 
-const app = express()
-const port = 3000
+// Load the environment variables from the project root directory (Parent of the current directory). This only needs to be ran once for the entire project.
+require('dotenv').config({ path: '../.env'});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//The backend should be listening on port 3000 within its container, but the container's port 3000 is mapped externally to 3001.
+// TL;DR the backend is running on port 3001 on the host machine. 
+app.listen(process.env.BACKEND_PORT, () => {
+  console.log(`Example app listening on port ${process.env.BACKEND_PORT}`)
 })
