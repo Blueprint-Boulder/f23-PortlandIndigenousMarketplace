9d481e02c3d857544bde512370cd29904c84950e
nh602 <nhenley602@gmail.com>
2023-11-02 22:30:41 +0000

    Backend now initializes database connection on startup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
 
-// Load the environment variables from the project root directory (Parent of the current directory). This only needs to be ran once for the entire project.
-require('dotenv').config({ path: '../.env'});
+// Load the environment variables from the .env file in the backend root folder 
+if(require('dotenv').config().parsed === undefined) 
+  throw new Error("Could not load environment variables");
+
+// Import the database object from the database.js file
+const db = require('./database');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,5 +15,5 @@ app.get('/', (req, res) => {
 //The backend should be listening on port 3000 within its container, but the container's port 3000 is mapped externally to 3001.
 // TL;DR the backend is running on port 3001 on the host machine. 
 app.listen(process.env.BACKEND_PORT, () => {
-  console.log(`Example app listening on port ${process.env.BACKEND_PORT}`)
+  console.log(`PIM backend app listening on port ${process.env.BACKEND_PORT}`)
 })
