c481543d30481b440dc5d5945545a70eb185051f
Ethan Epperson <etep9497@colorado.edu>
2023-11-02 20:06:36 -0600

    added links to home, insta, and facebook

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/footer.css"
+
+export default function footer() {
+    return(
+        <div className="Footer-links">
+            <a id="home" href="https://indigenousmarketplace.org/">PIM Home Page</a>
+            <a id="insta" href="https://www.instagram.com/indigenousmrktplc/">PIM Instagram</a>
+            <a id="fb" href="https://www.facebook.com/indigenousmrktplc">PIM Facebook</a>
+        </div>
+
+    )
+}
