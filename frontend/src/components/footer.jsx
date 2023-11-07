cbb76007d87b394bac0558cbdacbb0cad4f86c82
Ethan Epperson <etep9497@colorado.edu>
2023-11-06 18:36:53 -0700

    added icon for facebook, font awesome

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "../styles/footer.css"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 export default function footer() {
     return(
         <div className="Footer-links">
-            <a id="home" href="https://indigenousmarketplace.org/">PIM Home Page</a>
-            <a id="insta" href="https://www.instagram.com/indigenousmrktplc/">PIM Instagram</a>
-            <a id="fb" href="https://www.facebook.com/indigenousmrktplc">PIM Facebook</a>
+            <a className ="Link" href="https://indigenousmarketplace.org/">home</a>
+            <a className ="Link" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon="fa-brands fa-instagram" /></a>
+            <a className ="Link" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faFacebook} /></a>
         </div>
 
     )
