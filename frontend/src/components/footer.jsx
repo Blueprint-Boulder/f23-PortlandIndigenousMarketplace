726e263e5595edcf1c8318fa04399e090e273e00
Ethan Epperson <etep9497@colorado.edu>
2023-11-07 17:11:00 -0700

    got frontend back to working, adding to footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import "../styles/footer.css"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
-export default function footer() {
+export default function Footer() {
     return(
         <div className="Footer-links">
             <a className ="Link" href="https://indigenousmarketplace.org/">home</a>
-            <a className ="Link" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon="fa-brands fa-instagram" /></a>
+            <a className ="Link" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faInstagram} /></a>
             <a className ="Link" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faFacebook} /></a>
         </div>
-
+        // change ordering of files
+            // have home icon in middle
+            // center text
+            // maybe add line above
     )
 }
