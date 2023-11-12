091ffd988fd34354ca09dbe7ad264ac402727c4c
Ethan Epperson <etep9497@colorado.edu>
2023-11-12 15:50:21 -0700

    deciding to restyle footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,9 +8,13 @@ import {faHouse} from '@fortawesome/free-solid-svg-icons'
 export default function Footer() {
     return(
         <div className="Footer-links">
-            <a className ="link-style" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faSquareInstagram} /></a>
-            <a className="link-style" href="https://indigenousmarketplace.org/"><FontAwesomeIcon icon={faHouse}/></a>
-            <a className ="link-style" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faSquareFacebook} /></a>
+            <div className="Follow-us">
+                <div className="Socials">
+                    <a className ="social-links" id="insta" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faSquareInstagram} /></a>
+                    <a className ="social-links" id="facebook" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faSquareFacebook} /></a>
+                </div>
+            </div>
+                <a className="pim-link" href="https://indigenousmarketplace.org/"><FontAwesomeIcon icon={faHouse}/></a>
         </div>
         // change ordering of files
             // have home icon in middle
