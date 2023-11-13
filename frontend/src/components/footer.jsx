1396f2c86761a26bd8ca12eb376ca1c1ada56221
Ethan Epperson <etep9497@colorado.edu>
2023-11-12 19:53:53 -0700

    updated footed

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,13 +8,15 @@ import {faHouse} from '@fortawesome/free-solid-svg-icons'
 export default function Footer() {
     return(
         <div className="Footer-links">
-            <div className="Follow-us">
-                <div className="Socials">
-                    <a className ="social-links" id="insta" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faSquareInstagram} /></a>
-                    <a className ="social-links" id="facebook" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faSquareFacebook} /></a>
-                </div>
+            <div id="footer-text">Follow Us:</div>
+            <div className="Socials">
+                <a id="insta" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faSquareInstagram} /></a>
+                <a id="facebook" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faSquareFacebook} /></a>
             </div>
-                <a className="pim-link" href="https://indigenousmarketplace.org/"><FontAwesomeIcon icon={faHouse}/></a>
+            <div className="pim-link">
+                <div id="homepage-link" >Homepage:</div>
+                <a id="pim-home" href="https://indigenousmarketplace.org/"><FontAwesomeIcon icon={faHouse}/></a>
+            </div>
         </div>
         // change ordering of files
             // have home icon in middle
