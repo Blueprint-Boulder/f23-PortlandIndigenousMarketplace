c31797c305fd5ba436784c48671bc5dbb215aeb8
Ethan Epperson <etep9497@colorado.edu>
2023-11-13 18:55:55 -0700

    updated styling

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -7,25 +7,10 @@ import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
     return(
-        // <div className="Footer-links">
-        //     <div id="footer-text">Follow Us:</div>
-        //     <div className="Socials">
-        //         <a id="insta" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faSquareInstagram} /></a>
-        //         <a id="facebook" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faSquareFacebook} /></a>
-        //     </div>
-        //     <div className="pim-link">
-        //         <div id="homepage-link" >Homepage:</div>
-        //         <a id="pim-home" href="https://indigenousmarketplace.org/"><FontAwesomeIcon icon={faHouse}/></a>
-        //     </div>
-        // </div>
-        // change ordering of files
-            // have home icon in middle
-            // center text
-            // maybe add line above
         <div id="Footer">
             <div id="Footer-content">
                 <a className="social-links" href="https://www.instagram.com/indigenousmrktplc/"><FontAwesomeIcon icon={faSquareInstagram} /></a>
-                <a className="social-links p-1" href="https://indigenousmarketplace.org/">Homepage</a>
+                <a className="social-links p-1 underline" href="https://indigenousmarketplace.org/">Homepage</a>
                 <a className="social-links" id="social-links" href="https://www.facebook.com/indigenousmrktplc"><FontAwesomeIcon icon={faSquareFacebook} /></a>
             </div>
 
