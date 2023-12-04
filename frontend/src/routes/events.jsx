5308e26079db01fe4550ca96f6153c42a6dd99df
Ethan Epperson <etep9497@colorado.edu>
2023-12-04 16:48:30 -0700

    updates to button and such

--- frontend/src/routes/events.jsx
+++ frontend/src/routes/events.jsx
@@ -5,19 +5,20 @@ export default function Events() {
   const [about, setAbout] = useState(false);
   return (
     <div className="content-center w-auto h-auto">
-      <div className="flex flex-col m-1 my-2">
+      <div alt="Event-content" className="flex flex-col m-2 my-2">
         <img src={logo} alt='Event-logo' className='w-40 h-40 self-start border border-black rounded-xl'/>
-        <div className="text-2xl font-bold tracking-wide">Event Title</div>
-        <div className='flex flex-col'>
-          <label>About
-            <select className="self-start m-2 bg-opacity-100" onChange={(e) => setAbout(e.target.value)}>{about}</select>
-          </label>
-          <div>Location</div>
-          <div>Date+Time</div>
-          <button
-            className="text-gray-800 font-semibold py-2 px-1 border border-gray-400 rounded shadow"
-            // onClick={(setRegistered(registered))}
-          >Register</button>
+        <div alt="Event-title" className="text-2xl font-bold tracking-wide mt-2">Event Title</div>
+        <label>About
+          <select className="self-start m-2 bg-opacity-100" onChange={(e) => setAbout(e.target.value)}>{about}</select>
+        </label>
+        <div>Location</div>
+        <div>Date+Time</div>
+        <button
+          className="text-gray-800 font-semibold py-2 px-1 border border-gray-600 rounded w-24 hover:bg-gray-800 hover:text-gray-200 hover:border-gray-200"
+          // onClick={(setRegistered(registered))}
+        >Register</button>
+        <hr className=" border-gray-500 m-8 w-92"/>
+        <div alt='Attending Vendors'>
         </div>
       </div>
     </div>
