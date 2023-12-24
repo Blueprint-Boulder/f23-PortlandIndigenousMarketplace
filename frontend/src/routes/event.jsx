7c1541fb0df20506c772d9219a9112595cc36a64
Ethan Epperson <etep9497@colorado.edu>
2023-12-24 12:44:25 -0700

    reclone

--- frontend/src/routes/event.jsx
+++ frontend/src/routes/event.jsx
@@ -20,7 +20,7 @@ export default function Event({EventService}) {
   // b/c getEvent realistically has a prop location
   const {title, location, date, time, info} = EventService.getEventById(1);
   // const vendorList = RegisterEventService[id].getVendors();
-  const vendorImages = [logo, bLogo];
+  const vendorImages = [logo, bLogo, logo, bLogo, logo, bLogo, logo, bLogo, logo, bLogo, logo, bLogo];
 
   const toggleVendor = () => {
     setVendorOpen(!vendorOpen);
@@ -31,7 +31,7 @@ export default function Event({EventService}) {
   }
   return (
     <div id="Event-content" className={`overflow-scroll w-full h-[80vh] flex flex-col items-center ${vendorOpen ? 'blur-xl z-2' : ''}`}>
-      <button alt='back-arrow' className='mx-2 object-left-top fixed ' onClick={() => navigate(-1)}><FontAwesomeIcon icon={faAnglesLeft}/></button>
+      <button alt='back-arrow' className='self-start ml-2 fixed ' onClick={() => navigate(-1)}><FontAwesomeIcon icon={faAnglesLeft}/></button>
       <img src={logo} alt='Event-logo' className='w-2/3 bg-black py-0 bg-clip-padding rounded-xl'/>
       <div alt="Event-title" className="text-2xl mt-4 font-bold tracking-wide">{title}</div>
       <div alt='Event-about' className='flex flex-row mt-2'>
@@ -48,20 +48,21 @@ export default function Event({EventService}) {
         onClick={() => handleRegister()}
       >Register</button>
       <hr className='mt-3 mb-2 border-t-2 border-gray-600 w-3/4'/>
-      <div alt='Attending Vendors' className='mt-1 text-xl font-bold tracking-wide'>Attending Vendors</div>
-      <button alt="attendance" onClick={toggleVendor} className="mt-4 text-gray-800 font-semibold py-2 px-1 border border-gray-600 rounded w-24">See attending vendors</button>
-      <div alt='vendorImages' className='grid-cols-auto w-2/3 overflow-x-scroll rounded border-2'>
+      <div alt='Attending Vendors' className='mt-1 text-xl font-bold italic tracking-wide'>Attending Vendors</div>
+      <div alt='vendorImages' className='grid grid-cols-5 gap-1 w-2/3 mt-2 rounded border-2'>
         {vendorImages.map((vendorImg) => (
-          <img key={vendorImg} src={vendorImg} alt={vendorImg} className='w-4 h-4'/>
+          <img key={vendorImg} src={vendorImg} alt={vendorImg} onClick={toggleVendor} className='w-16 h-14'/>
         ))}
       </div>
     </div>
   );
 }
 
+// <button alt="attendance" onClick={toggleVendor} className="mt-4 text-gray-800 font-semibold py-2 px-1 border border-gray-600 rounded w-24">See attending vendors</button>
 // put the images into a container, try to make it sideways scrollable where images are cutoff by border of div
 // resize images
 
 Event.propTypes = {
   EventService: PropTypes.func.isRequired,
 };
+
