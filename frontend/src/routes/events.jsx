74c540ae7d48b74a855eea73196c11f519ad13d6
Eric Fithian <eric.fithian@icloud.com>
2024-01-26 13:11:06 -0700

    finished ui

--- frontend/src/routes/events.jsx
+++ frontend/src/routes/events.jsx
@@ -10,9 +10,9 @@ export default function Events({EventService}) {
     <div className="bg-white shadow-lg rounded-lg p-4 max-w-sm mx-auto bm-4">
       <div className="mt-2">
         <div className="text-lg font-semibold text-gray-900">{event.name}</div>
-        <div className="text-gray-500">{event.description}</div>
-        <div className="mt-3 text-gray-700">{event.date} • {event.startTime} - {event.endTime}</div>
-        <div className="mt-1 text-sm text-gray-500">
+        <div className="text-grey-5">{event.description}</div>
+        <div className="mt-3 text-grey-5">{event.date} • {event.startTime} - {event.endTime}</div>
+        <div className="mt-1 text-sm text-grey-5">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 2C8.13401 2 5 5.13401 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13401 15.866 2 12 2ZM12 11C10.3431 11 9 9.65685 9 8C9 6.34315 10.3431 5 12 5C13.6569 5 15 6.34315 15 8C15 9.65685 13.6569 11 12 11Z" />
         </svg>
@@ -24,7 +24,7 @@ export default function Events({EventService}) {
 
   return (
     // display in a single column
-    <div className=' w-screen pl-2 pr-2 flex flex-col items-left justify-between gap-2 mb-10'>
+    <div className='w-full mx-auto flex flex-col justify-center pb-16'>
       <h1 className='color-white text-2xl text-center my-3 font-semibold'>Events</h1>
       <div className='flex flex-col space-y-4'>
         {
