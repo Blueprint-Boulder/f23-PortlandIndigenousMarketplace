be70a457b98bf76f64892bf2290e36843ff43c40
nh602 <nhenley602@gmail.com>
2024-01-28 07:31:07 +0000

    Added middleware functions for handling admin cookie authentication.

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 
-// This middleware will be used to sign the token after a user logs in
+// TODO: Set cookies to secure when https is ready?
+
+// This middleware will be used to sign the token after a vendor logs in
 const signToken = async (req, res, next) => {
   // Sign the token with JWT_SECRET
   const token = jwt.sign(res.locals.vendor, process.env.JWT_SECRET);
@@ -10,7 +12,7 @@ const signToken = async (req, res, next) => {
   next();
 };
 
-// This middleware will be used to verify the token sent by the client
+// This middleware will be used to verify the token sent by the vendor
 const verifyToken = async (req, res, next) => {
   // Retrieve the token from the cookie
   const token = req.cookies.auth;
@@ -27,4 +29,47 @@ const verifyToken = async (req, res, next) => {
   });
 };
 
-module.exports = {signToken, verifyToken};
+// This middleware will be used to sign the token after an admin logs in
+const signAdminToken = async (req, res, next) => {
+  // Sign the token with JWT_SECRET
+  const token = jwt.sign(res.locals.admin, process.env.JWT_SECRET);
+
+  // Return the token in a cookie
+  res.cookie('auth_pim', token, {httpOnly: true, secure: false});
+
+  next();
+};
+
+const verifyAdminToken = async (req, res, next) => {
+  // Retrieve the token from the cookie
+  const token = req.cookies.auth_pim;
+
+  // Verify the token with JWT_SECRET
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({message: 'Unauthorized'});
+    } else {
+      // Add the decoded object to res.locals
+      res.locals.admin = decoded;
+      next();
+    }
+  });
+};
+
+// Returns a middleware to verify the user has the correct permissions.
+// Setting a route that only admins can access.
+// USAGE: router.get('/route', verify('admin'), controller);
+const verify = (privilege) => {
+  if (privilege === 'admin') {
+    // Check the admin token
+    return verifyAdminToken;
+  } else if (privilege === 'vendor') {
+    // Check the vendor token
+    return verifyToken;
+  } else {
+    // You shouldn't need to set privilege for any other use case.
+    throw new Error('Privilege should be either vendor or admin.');
+  }
+};
+
+module.exports = {signToken, verifyToken, verifyAdminToken, signAdminToken, verify};
