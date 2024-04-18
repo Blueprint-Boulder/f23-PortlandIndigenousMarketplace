13e4157661a61888dad32718d6673b66478da731
Nicholas Henley <nhenley602@gmail.com>
2024-04-18 17:08:06 +0000

    Added headers already sent check to sendSuccessResponse

--- backend/middleware/successResponse.js
+++ backend/middleware/successResponse.js
@@ -1,16 +1,21 @@
 // successResponse.js
 function sendSuccessResponse(req, res) {
-  if (res.locals.data) {
-    // console.log('Data in res.locals', res.locals.data);
-    res.status(200).json(res.locals.data);
-  } else {
-    // Fallback if no data is set in res.locals, but middleware is called
-    res.status(200).json(
-        {
-          status: 'success',
-          message: 'Operation completed successfully.',
-        },
-    );
+  if(!res.headersSent){
+    if (res.locals.data) {
+      // console.log('Data in res.locals', res.locals.data);
+      res.status(200).json(res.locals.data);
+    } else {
+      // Fallback if no data is set in res.locals, but middleware is called
+      res.status(200).json(
+          {
+            status: 'success',
+            message: 'Operation completed successfully.',
+          },
+      );
+    }
+  }
+  else {
+    console.error("sendSuccessResponse: Headers already sent!");
   }
 }
 
