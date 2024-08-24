60bb8cc0278dcc454a13658ee2e1133cac03df9b
Nicholas Henley <nhenley602@gmail.com>
2024-08-24 19:03:52 +0000

    Refactored upload photo modal to use Modal component

--- frontend/src/components/UploadPhotoModal.jsx
+++ frontend/src/components/UploadPhotoModal.jsx
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 
+import Modal from './Modal';
 import {Context} from '../services/context';
 
 export default function UploadPhotoModal({vendorId, vendorService, showUploadModal, setShowUploadModal}) {
@@ -30,13 +31,12 @@ export default function UploadPhotoModal({vendorId, vendorService, showUploadMod
   };
 
   return (<>
-    <div className='absolute backdrop-blur-md w-full h-full z-10'></div>
-    <div className='absolute bg-white z-20 rounded-lg shadow-2xl flex flex-col gap-5 p-10 items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 h-1/4'>
-      <h2>Upload Profile Picture</h2>
+    <Modal backgroundColor={'bg-greywhite'} blurOnClick={toggle}>
+      <h1 className="font-bold">Upload Profile Picture</h1>
       <form>
         <input type="file" accept="image/*" onChange={fileUpload}></input>
       </form>
       <button onClick={toggle}>Close</button>
-    </div>
+    </Modal>
   </>);
 }
