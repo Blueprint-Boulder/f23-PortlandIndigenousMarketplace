483f217d24b257f09c378dab67811144d563ff2f
Nicholas Henley <nhenley602@gmail.com>
2024-08-24 16:04:55 +0000

    Replaced events page edit event modal with one created for event page.

--- frontend/src/routes/events.jsx
+++ frontend/src/routes/events.jsx
@@ -3,78 +3,18 @@ import PropTypes from 'prop-types';
 import {Context} from '../services/context';
 import {Link} from 'react-router-dom';
 import FooterPad from '../components/footerpad';
-// import moment from 'moment';
-
-
-import DatePicker from 'react-datepicker';
-
-import 'react-datepicker/dist/react-datepicker.css';
-
-function EventModal({editEvent, handleSubmit, closeModal, currEvent}) {
-  const [eventInfo, setEventInfo] = useState({name: '', description: '', location: '', starttime: new Date(), endtime: new Date(), vendorCapacity: 0});
-  useEffect(() => {
-    if (editEvent) {
-      currEvent.starttime = new Date(currEvent.startDate);
-      currEvent.endtime = new Date(currEvent.endDate);
-      console.log('Curr event', currEvent);
-      setEventInfo(currEvent);
-    }
-  }, [editEvent]);
-
-
-  return (
-    <form action="" className='grid z-50 gap-2 p-4 lg:grid-cols-12 grid-cols-3 left-0 right-0 top-0 bottom-0 mt-auto mb-auto h-4/6 lg:ml-auto lg:mr-auto rounded-sm lg:w-8/12 w-full fixed bg-grey-1'
-      onSubmit={() => handleSubmit(eventInfo)}>
-      <div className='lg:col-span-2 col-span-1 my-auto'>Event Name:</div>
-      <input className='lg:col-span-10 col-span-2 rounded-md shadow-md p-1' required type="text" id='eventName' name='eventName' value={eventInfo.name} onChange={(e) => setEventInfo({...eventInfo, name: e.target.value})} />
-      <div className='lg:col-span-2 col-span-1 row-span-2'>Description:</div>
-      <textarea className='lg:col-span-10 col-span-2 row-span-2 rounded-md shadow-md p-1' required type="text" id='description' name='description' value={eventInfo.description} onChange={(e) => setEventInfo({...eventInfo, description: e.target.value})} />
-      <div className='lg:col-span-2 col-span-1 my-auto'>Location:</div>
-      <input className='lg:col-span-10 col-span-2 rounded-md shadow-md p-1' required type="text" id='location' name='location' value={eventInfo.location} onChange={(e) => setEventInfo({...eventInfo, location: e.target.value})} />
-      <div className='lg:col-span-2 col-span-1 my-auto'>Start Time:</div>
-      <DatePicker
-        id='start-time'
-        name='start-time'
-        selected={eventInfo.starttime}
-        onChange={(dateTime) => setEventInfo({...eventInfo, starttime: dateTime})}
-        showTimeSelect
-        timeIntervals={15}
-        required
-        wrapperClassName='lg:col-span-10 col-span-2 rounded-md shadow-md p-2 bg-white '
-        className='lg:col-span-10 w-full rounded-md h-max  absolute top-0 bottom-0 bg-white mt-auto mb-auto p-1'
-        timeCaption="Time"
-        dateFormat="MMMM d, yyyy h:mm"
-      />
-      <div className='lg:col-span-2 col-span-1 my-auto'>End Time:</div>
-      <DatePicker
-        id='end-time'
-        name='end-time'
-        selected={eventInfo.endtime}
-        onChange={(dateTime) => setEventInfo({...eventInfo, endtime: dateTime})}
-        showTimeSelect
-        timeIntervals={15}
-        wrapperClassName='lg:col-span-10 col-span-2 rounded-md shadow-md p-2 bg-white '
-        className='lg:col-span-10 w-full h-max rounded-md absolute top-0 bottom-0 bg-white mt-auto mb-auto p-1'
-        required
-        timeCaption="Time"
-        dateFormat="MMMM d, yyyy h:mm"
-      />
-      <div className='lg:col-span-2 col-span-1 my-auto'>Capacity:</div>
-      <input className='lg:col-span-10 col-span-2 rounded-md shadow-md p-1' type="text" id='vendor-capacity' name='location' value={eventInfo.vendorCapacity} onChange={(e) => setEventInfo({...eventInfo, vendorCapacity: e.target.value})} />
-      <button type='submit' className='bg-blue lg:col-span-8 col-span-2 rounded-md shadow-sm text-white p-1 '>{editEvent ? 'Save Changes' : 'Add Event'}</button>
-      <button type='button' className='bg-red lg:col-span-4 col-span-1 rounded-md shadow-sm text-white p-1 ' onClick={() => closeModal()}>Cancel</button>
-    </form>
-
-  );
-}
+import EditEventModal from '../components/EditEventModal.jsx';
+import Event from '../objects/Event';
 
 export default function Events({eventService}) {
   const [events, setEvents] = useState([]);
   const [error, setError] = useState('');
   const {user} = useContext(Context);
-  const [modal, setModal] = useState(false);
-  const [editEvent, setEditEvent] = useState(false);
+
+  // Add / Edit event modal state
+  const [showEditModal, setShowEditModal] = useState(false);
   const [currEvent, setCurrEvent] = useState(null);
+  const [isNewEvent, setIsNewEvent] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -94,42 +34,6 @@ export default function Events({eventService}) {
 
     fetchEvents();
   }, [eventService, currEvent]);
-
-  async function handleSubmit(event) {
-    // if we are editing an event we need to update the event
-    console.log('Event', event);
-    if (editEvent) {
-      setEditEvent(true);
-      setModal(false);
-      const res = await eventService.updateEvent(currEvent.eventId, event);
-      console.log('Res status', res.status);
-      if (res !== undefined) {
-        console.log('Event updated successfully');
-        // const updatedEvents = await eventService.getAllEvents();
-        // setEvents(updatedEvents);
-      } else {
-        console.error('Failed to update event');
-      }
-      setCurrEvent(null);
-    } else { // else we are creating a new event
-      setEditEvent(false);
-      setModal(false);
-      const res = await eventService.createEvent(event);
-      if (res !== undefined) {
-        console.log('Event added successfully');
-        // const updatedEvents = await eventService.getAllEvents();
-        // setEvents(updatedEvents);
-      } else {
-        console.error('Failed to add event');
-      }
-      setCurrEvent(null);
-    }
-  };
-
-  function closeModal() {
-    setCurrEvent(null);
-    setModal(false);
-  }
 
   const eventDisplay = (event) => {
     const start = new Date(`${event.startDate} ${event.starttime}`);
@@ -155,7 +59,7 @@ export default function Events({eventService}) {
           </Link>
           {
             user && user.isadmin && <button onClick={() => {
-              setEditEvent(true); setModal(true); setCurrEvent(event);
+              setIsNewEvent(false); setCurrEvent(event); setShowEditModal(true);
             }} className='text-blue text-md p-2'>Edit</button>
           }
         </div>
@@ -168,28 +72,23 @@ export default function Events({eventService}) {
     return (
       <div className='w-full relative mx-auto flex flex-col justify-center pb-16'>
         <h1 className='color-white text-2xl text-center my-3 font-semibold'>No Events Found</h1>
-        {!modal && user && user.isadmin && <button className='bg-white hover:bg-blue shadow-sm absolute right-0 text-black w-max m-2 p-2 rounded-lg'
+        {!showEditModal && user && user.isadmin && <button className='bg-white hover:bg-blue shadow-sm absolute right-0 text-black w-max m-2 p-2 rounded-lg'
           onClick={() => {
-            setEditEvent(false); setModal(true);
+            setIsNewEvent(true); setCurrEvent(new Event(undefined, "", "", new Date(), new Date(), "", 0)); setShowEditModal(true);
           }}>Add Event</button>}
-        {modal && (
-          <EventModal editEvent={editEvent} handleSubmit={handleSubmit} closeModal={closeModal} currEvent={currEvent}/>
-        )}
       </div>
     );
   }
 
   return (
     <>
-      {modal && (
-        <EventModal editEvent={editEvent} handleSubmit={handleSubmit} closeModal={closeModal} currEvent={currEvent}/>
-      )}
-      <div className={`${modal && 'blur-sm'} w-full mx-auto flex flex-col justify-center pb-16`}>
+      {showEditModal && <EditEventModal event={currEvent} eventService={eventService} setShowEditModal={setShowEditModal} isNewEvent={isNewEvent}/>}
+      <div className={`w-full mx-auto flex flex-col justify-center pb-16`}>
         
         <div className={`flex flex-row ${user && user.isadmin ? 'justify-around' : 'justify-center'}`}>
-          {!modal && user && user.isadmin && <button className='bg-greywhite hover:bg-blue shadow-sm absolute right-0 text-black w-max m-2 p-2 rounded-lg'
+          {!showEditModal && user && user.isadmin && <button className='bg-greywhite hover:bg-blue shadow-sm absolute right-0 text-black w-max m-2 p-2 rounded-lg'
             onClick={() => {
-              setEditEvent(false); setModal(true);
+              setIsNewEvent(true); setCurrEvent(new Event(undefined, "", "", new Date(), new Date(), "", 0)); setShowEditModal(true); 
             }}>Add Event</button>}
 
           <h1 className='color-white text-2xl text-center my-3 font-semibold'>Events</h1>
@@ -213,11 +112,4 @@ Events.propTypes = {
     createEvent: PropTypes.func.isRequired,
     updateEvent: PropTypes.func.isRequired,
   }).isRequired,
-};
-
-EventModal.propTypes = {
-  editEvent: PropTypes.bool.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  closeModal: PropTypes.func.isRequired,
-  currEvent: PropTypes.object,
 };
